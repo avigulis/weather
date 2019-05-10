8ebf714b16f0fa58bc566f688243f9527d2a47d7
Ainars Vigulis <Ainars.Vigulis@4finance.com>
2019-05-10 07:50:06 +0300

    caching configuration, incorrect initialization. Explicit define cache type and names.

--- src/main/java/org/assignment/weather/CacheConfiguration.java
+++ src/main/java/org/assignment/weather/CacheConfiguration.java
@@ -1,7 +1,9 @@
 package org.assignment.weather;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 
 @EnableCaching
+@Configuration
 public class CacheConfiguration {
 }
